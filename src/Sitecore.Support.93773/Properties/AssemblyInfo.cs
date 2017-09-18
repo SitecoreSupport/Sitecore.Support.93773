5ee9b714c44d40ee639a499574d950d4475ea50e
DK\AEC <aec@sitecore.net>
2017-09-18 15:11:12 +0300

    Commit patch

---
+++ src/Sitecore.Support.93773/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.93773")]
+[assembly: AssemblyProduct("Sitecore.Support.93773")]
+[assembly: ComVisible(false)]
